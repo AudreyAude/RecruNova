ec45d5e18d8d4349e7520b8bbe9967672cedb53f
AudreyAude <audreymakoutsing5@gmail.com>
2025-04-11 19:39:04 -0400

    .

--- Backend/app.py
+++ Backend/app.py
@@ -7,14 +7,15 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
-from .model import User
+from .model import User,log,offre
 from .function import password_hash,password_verify
+from datetime import datetime
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 app=FastAPI()
 load_dotenv()
-# app.add_middleware(SessionMiddleware,Secret_key=os.getenv("secret_key"),max_age=60)
+app.add_middleware(SessionMiddleware,Secret_key=os.getenv("secret_key"),max_age=60)
 templates_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"templates"))
 templates=Jinja2Templates(directory=templates_dir)
 
@@ -53,15 +54,77 @@ async def connect_empl(request:Request, E:User):
 
 
     if resultat:
-        return{"message : un compte utilise deja cet email."}
+        return templates.TemplateResponse("User.html",{"request":request })
     else:
         
        y= password_hash(E.password)
+
+       E.date_inscription = datetime.now()
        sql =""" 
        INSERT INTO  Recrunova.Recrut.Users ( nom,prenom,nom_entreprise,role,password,Tel,date_inscription,Email,Cv )
-       values (%s,%s,%s,%s,%s,%s,%s,%s)
+       values (%s,%s,%s,%s,%s,%s,%s,%s,%s)
        
        """
+       params=[E.nom,E.prenom,E.nom_entreprise,E.role,y,E.Tel,E.date_inscription,E.Email]
+       x=cursor.execute(sql,params)
+
+
+       return templates.TemplateResponse("User.html",{"request":request })
+    
+
+@app.get("/login")
+async def  Login (request:Request):
+    error=request.session.get("error",None)
+    if error:
+        return templates.TemplateResponse("login.html",{"request":request,"error":error})
+    return templates.TemplateResponse("login.html",{"request":request})
+
+
+@app.post("/login")
+
+async def Login(L:log,request:Request):
+    sql="SELECT *  FROM  Recrunova.Recrut.Users where Email=%s"
+    params=[L.Email]
+    cursor.execute(sql,params)
+    resultat=cursor.fetchone()
+    
+    if resultat:
+        x= password_verify(L.password,resultat[5])
+
+        if x :
+          s={
+              "nom":resultat[1],
+              "prenom":resultat[2],
+              "Email":resultat[8],
+              "Role":resultat[4],
+            }
+          response = RedirectResponse(url='/', status_code=302)
+          
+          request.session["user"] = s
+          
+          return response
+        
+        else:
+          request.session["error"] = "mot de passe incorrect"
+          
+        return RedirectResponse(url='/login', status_code=302)
+      
+    else: 
+        request.session["error"] = "email introuvable "
+
+        return RedirectResponse(url='/login', status_code=302)
+    
+
+@app.get("/ajout_offre")
+async def Ajout_offre(A:offre,request:Request):
+    sql=" INSERT INTO Recrunova.Recrut.Offres( titre,langue,salaire,description,competences,type_poste,horaire,avantages)" \
+    " values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+    params=[ A.titre,A.langue,A.salaire,A.description,A.competences,A.type_poste,A.horaire,A.avantages]
+    cursor.execute(sql,params)
+
+    return templates.TemplateResponse("")
+
 
 
 
--- Backend/model.py
+++ Backend/model.py
@@ -12,3 +12,18 @@ class User(BaseModel):
     date_inscription:str
     Email :str
     # Cv 
+
+
+class log(BaseModel):
+    Email :str
+    password :str
+
+class offre(BaseModel):
+    titre :str
+    langue :str
+    salaire :str
+    description :str
+    competences :str
+    type_poste :str
+    horaire :str
+    avantages :str
