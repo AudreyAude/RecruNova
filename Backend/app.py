33b7222884a61ed030e0b6c564b531386e7addc6
AudreyAude <audreymakoutsing5@gmail.com>
2025-04-11 16:13:39 -0400

    .

--- Backend/app.py
+++ Backend/app.py
@@ -7,7 +7,8 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
-
+from .model import User
+from .function import password_hash,password_verify
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
@@ -29,16 +30,57 @@ Connect = sc.connect(
     database=os.getenv("snowflake_database"),
 )
 
+cursor=Connect.cursor() 
+
 @app.get("/home")
 async def home_page(request:Request):
     
     return templates.TemplateResponse("home.html",{"request":request })
 
+
 @app.get("/sing_up")
-async def connect_empl(resquest:Request):
-    sql = "SELECT * FROM  Recrunova.Recrut.Users where Email=%s"
-    params=[username]
+async def  connect_emp(request:Request):
     
+    return templates.TemplateResponse("User.html",{"request":request })
+
+
+@app.post("/sing_up")
+async def connect_empl(request:Request, E:User):
+    sql = "SELECT * FROM  Recrunova.Recrut.Users where Email=%s"
+    params=[E.Email]
+    cursor.execute(sql,params)
+    resultat=cursor.fetchone()
+
+
+    if resultat:
+        return{"message : un compte utilise deja cet email."}
+    else:
+        
+       y= password_hash(E.password)
+       sql =""" 
+       INSERT INTO  Recrunova.Recrut.Users ( nom,prenom,nom_entreprise,role,password,Tel,date_inscription,Email,Cv )
+       values (%s,%s,%s,%s,%s,%s,%s,%s)
+       
+       """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__=="__main__":
